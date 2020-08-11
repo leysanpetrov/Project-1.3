14863e2c7172c04aebb5f4af9041badc760259e9
Leysan Petrov <leysan707@gmail.com>
2020-08-11 16:58:26 +0300

    fix block 5

--- index.js
+++ index.js
@@ -34,3 +34,24 @@ close.addEventListener('click', function () {
   seeAll.classList.remove('seeAll_display_none');
 })
 
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+})
